920285c12a485b759dc36fdc611d1c886a5da681
bharatpostnews <development@bharatpostnews.com>
2022-03-31 11:35:27 +0530

    test

--- src/Layouts/Header/headerOne.js
+++ src/Layouts/Header/headerOne.js
@@ -4,7 +4,7 @@ import SocialIcons from "../../components/header/socialIcons";
 
 export default function HeaderOne({ LinksFetch }) {
   return (
-    <div className=" border-b-2 py-1 border-third ">
+    <div className=" hidden xl:block lg:block 2xl:block border-b-2 py-1 border-third ">
       <div className="mx-40 flex justify-between">
         <CurrentTimeDate />
         <SocialIcons LinksFetch={LinksFetch} />
