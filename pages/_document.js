ba0fa5625af0dc8d6197d2ae7713e76efbda1d13
Rohit Mishra <99918800+bharatpostnews@users.noreply.github.com>
2022-06-30 17:53:09 +0530

    Revert "added gtag"
    This reverts commit f0579139d985ca5769e85f22c2bcdb3b5584b099.

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -11,17 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <Script
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MHFR578'); `,
-            }}
-          ></Script>
-
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
@@ -34,12 +22,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           />
         </Head>
         <body>
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MHFR578"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          ></noscript>
           <Main />
           <NextScript />
         </body>
